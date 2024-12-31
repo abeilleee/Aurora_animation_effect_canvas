fa5c97a02de69bb2b2127ef88f17a126c1b10b2d
May <abeillee@bk.ru>
2024-12-31 17:36:59 +0300

    feat: add style for canvas.b

--- script.js
+++ script.js
@@ -14,5 +14,7 @@ function setCanvas(){
     ctx = {
         a: canvas.a.getContext('2d'),
         b: canvas.b.getContext('2d'),
-    }
+    };
+    canvas.b.style = 'position: fixed; left: 0; top: 0; width: 100%; height: 100%';
+    container.appendChild(canvas.b);
 }
